16e21adfafd99027350de5b7d0ac360a446b14d2
Kevin <chenglu7701@gmail.com>
2024-06-26 11:37:46 +0800

    SetUp LoginViewController layout & UIView Extensions

--- CLMessenger/Controllers/Login/LoginViewController.swift
+++ CLMessenger/Controllers/Login/LoginViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "CLMessengerIcon")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,17 @@ class LoginViewController: UIViewController {
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
+        // Add subviews
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.frame.size.width/3
+        imageView.frame = CGRect(x: (view.frame.size.width-size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
     }
     
     @objc private func didTapRegister() {
