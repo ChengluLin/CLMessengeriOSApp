35781527c8b8dbebc9686d748aa0628b85607e0b
Kevin <chenglu7701@gmail.com>
2024-06-27 10:45:01 +0800

    Add LoginViewController layout

--- CLMessenger/Controllers/Login/LoginViewController.swift
+++ CLMessenger/Controllers/Login/LoginViewController.swift
@@ -9,12 +9,62 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
     private let imageView: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.image = UIImage(named: "CLMessengerIcon")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
+    
+    private let emailField: UITextField = {
+       let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "輸入Email..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+       let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "輸入密碼..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("登入", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,16 +76,34 @@ class LoginViewController: UIViewController {
                                                             target: self,
                                                             action: #selector(didTapRegister))
         // Add subviews
-        view.addSubview(imageView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
+
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.frame.size.width/3
-        imageView.frame = CGRect(x: (view.frame.size.width-size)/2,
-                                 y: 20,
+        scrollView.frame = view.bounds
+        let size = scrollView.width/5
+        imageView.frame = CGRect(x: (scrollView.width-size)/2,
+                                 y: 30,
                                  width: size,
                                  height: size)
+        emailField.frame = CGRect(x: 30,
+                                 y: imageView.bottom+50,
+                                  width: scrollView.width - 60,
+                                 height: 52)
+        passwordField.frame = CGRect(x: 30,
+                                 y: emailField.bottom+10,
+                                  width: scrollView.width - 60,
+                                 height: 52)
+        loginButton.frame = CGRect(x: 30,
+                                 y: passwordField.bottom+10,
+                                  width: scrollView.width - 60,
+                                 height: 52)
     }
     
     @objc private func didTapRegister() {
