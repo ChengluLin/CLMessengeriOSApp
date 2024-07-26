6fd2e3cfe6549ee0c8a55c10dd6f68a26d1785a1
Kevin <chenglu7701@gmail.com>
2024-07-26 17:26:44 +0800

    Add searchBar filter method and TableViewDelegate in NewConversationVC

--- CLMessenger/Controllers/NewConversationViewController.swift
+++ CLMessenger/Controllers/NewConversationViewController.swift
@@ -10,7 +10,13 @@ import JGProgressHUD
 
 class NewConversationViewController: UIViewController {
     
-    private let spinner = JGProgressHUD()
+    private let spinner = JGProgressHUD(style: .dark)
+    
+    private var users = [[String: String]]()
+    
+    private var results = [[String: String]]() // 過濾後結果
+    
+    private var hasFetched = false
     
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
@@ -27,15 +33,22 @@ class NewConversationViewController: UIViewController {
     
     private let noResultsLabel: UILabel = {
         let label = UILabel()
+        label.isHidden = true
         label.text = "查無結果..."
         label.textAlignment = .center
-        label.textColor = .green
+        label.textColor = .lightGray
         label.font = .systemFont(ofSize: 21, weight: .medium)
         return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(noResultsLabel)
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         view.backgroundColor = .white
         searchBar.delegate = self
         navigationController?.navigationBar.topItem?.titleView = searchBar
@@ -44,15 +57,108 @@ class NewConversationViewController: UIViewController {
         searchBar.becomeFirstResponder()
     }
     
-   @objc private func dismissSelf() {
-       dismiss(animated: true)
-       
+    @objc private func dismissSelf() {
+        dismiss(animated: true)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.tableView.frame = view.bounds
+        noResultsLabel.frame = CGRect(x: view.width/4,
+                                      y: (view.height-200)/2,
+                                      width: view.width/2,
+                                      height: 200)
     }
     
 }
 
-extension NewConversationViewController: UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard
+extension NewConversationViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = results[indexPath.row]["name"]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // 開始對話
     }
 }
+
+extension NewConversationViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text,
+              !text.isEmpty,
+              !text.replacingOccurrences(of: " ", with: "").isEmpty else {
+            return
+        }
+        searchBar.resignFirstResponder()
+        
+        results.removeAll()
+        spinner.show(in: view)
+        self.searchUsers(query: text)
+        
+    }
+    
+    func searchUsers(query: String) {
+        // 檢查firebase是否有資料
+        
+        if hasFetched {
+            // 如果有：過濾
+            filterUsers(with: query)
+        } else {
+            // 如果沒有：先取得資料再篩選
+            DatabaseManager.shared.getAllUsers { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                case .success(let usersCollection):
+                    self.hasFetched = true
+                    self.users = usersCollection
+                    self.filterUsers(with: query)
+                case .failure(let error):
+                    print("Failed to get users: \(error)")
+                }
+            }
+        }
+        
+    }
+    
+    func filterUsers(with term: String) {
+        // 最後更新UI
+        guard hasFetched else {
+            return
+        }
+        
+        self.spinner.dismiss()
+        
+        let results: [[String: String]] = self.users.filter {
+            guard let name = $0["name"]?.lowercased() else {
+                return false
+            }
+            
+            return name.hasPrefix(term.lowercased())
+        }
+        self.results = results
+        
+        updateUI()
+        
+    }
+
+    func updateUI() {
+        if results.isEmpty {
+            self.noResultsLabel.isHidden = false
+            self.tableView.isHidden = true
+        } else {
+            self.noResultsLabel.isHidden = true
+            self.tableView.isHidden = false
+            self.tableView.reloadData()
+        }
+    }
+}
+
